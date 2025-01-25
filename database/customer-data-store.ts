cf0216723630f016adccf1e9104e7211f7a40df2
Charaka V Mihiranga <charakavmihiranga@gmail.com>
2025-01-25 20:00:08 +0530

    Add customer management functionality: implement delete, update, and retrieve operations

--- database/customer-data-store.ts
+++ database/customer-data-store.ts
@@ -3,7 +3,6 @@ import Customer from "../model/Customer";
 
 const prisma = new PrismaClient();
 export async function addCustomer(customer: Customer) {
-    console.log(customer)
     try {
         return await prisma.customer.create({
             data: {
@@ -15,6 +14,46 @@ export async function addCustomer(customer: Customer) {
         });
     } catch (error) {
         console.error(`Error adding customer: ${error}`);
+        throw error;
+    }
+}
+export async function deleteCustomer(email: string) {
+    try {
+        return await prisma.customer.delete({
+            where: {
+                Email: email
+            }
+        });
+    } catch (error) {
+        console.error(`Error deleting customer: ${error}`);
+        throw error;
+    }
+}
+
+export async function updateCustomer( id: number, customer: Customer) {
+    try {
+        return await prisma.customer.update({
+            where: {
+                CustomerID: id
+            },
+            data: {
+                Name: customer.name,
+                Nic: customer.nic,
+                Email: customer.email,
+                Phone: parseInt(customer.phone)
+            }
+        });
+    } catch (error) {
+        console.error(`Error updating customer: ${error}`);
+        throw error;
+    }
+}
+
+export async function getCustomers() {
+    try {
+        return await prisma.customer.findMany();
+    } catch (error) {
+        console.error(`Error getting customers: ${error}`);
         throw error;
     }
 }
--- routes/customer-routes.ts
+++ routes/customer-routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import Customer from "../model/Customer";
-import {addCustomer} from "../database/customer-data-store";
+import {addCustomer, deleteCustomer, getCustomers, updateCustomer} from "../database/customer-data-store";
 
 // use the express.Router class to create modular, mountable route handlers
 const router = express.Router();
@@ -16,4 +16,36 @@ router.post("/add",async (req, res) => {
     }
 });
 
+router.delete("/remove/:email", async (req, res) => {
+    const email: string = req.params.email;
+    try {
+        const deletedCustomer = await deleteCustomer(email);
+        res.status(200).json(deletedCustomer);
+    } catch (e) {
+        console.log(`Error deleting customer: ${e}`);
+        res.status(500).send("Error deleting customer");
+    }
+});
+
+router.put("/update/:id", async (req, res) => {
+    const id: number = parseInt(req.params.id);
+    const customer: Customer = req.body as Customer;
+    try {
+        const updatedCustomer = await updateCustomer(id, customer);
+        res.status(200).json(updatedCustomer);
+    } catch (e) {
+        console.log(`Error updating customer: ${e}`);
+        res.status(500).send("Error updating customer");
+    }
+});
+
+router.get("/all", async (req, res) => {
+    try {
+        const customers = await getCustomers();
+        res.status(200).json(customers);
+    } catch (e) {
+        console.log(`Error getting customers: ${e}`);
+        res.status(500).send("Error getting customers");
+    }
+});
 export default router;
